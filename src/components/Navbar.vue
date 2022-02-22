7242b7962fab34aff1f0f1826971d16b722a3dca
Marco <msperl00@estudiantes.unileon.es>
2022-02-22 16:26:36 +0100

    fix css navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,21 +12,21 @@
                     <img class="ml-3 h-10" src="../../public/prismaticos.png" alt="prismaticos" />
                 </router-link>
             </a>
-            <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
+            <div class="hidden w-full md:block md:w-auto"  id="mobile-menu">
                 <ul
                     class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium"
                 >
                     <li>
                         <a
                             href="/"
-                            class="block py-2 pr-4 pl-3 text-white  md:p-0 dark:text-white text-slate-400 hover:text-sky-400"
+                            class="block py-2 pr-4 pl-3 text-gray-700  text-lg border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                             aria-current="page"
                         >Home</a>
                     </li>
                     <li>
                         <a
                             href="/about"
-                            class="block py-2 pr-4 pl-3 text-white  md:p-0 dark:text-white text-slate-400 hover:text-sky-400"
+                            class="block py-2 pr-4 pl-3 text-gray-700 text-lg border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                         >About</a>
                     </li>
                 </ul>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,16 +7,17 @@ import ButtonRepo from '@/components/ButtonRepo.vue'
       class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-6 lg:px-8 md:flex md:items-center md:justify-between md:py-6 md:px-8"
     >
       <h2
-        class="text-3xl font-extrabold md:text-center leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
+        class="text-3xl font-extrabold  leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
       >
         Ready to track covid?
         <br />
       </h2>
-      <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+      <!-- flex-shrink-0 and mt-0 -->
+      <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0 md:mt-0 md:flex-shrink-0">
         <div class="inline-flex rounded-md shadow">
           <router-link
             to="/trackerDetails"
-            class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
+            class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading- text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
             >Go track</router-link
           >
         </div>
